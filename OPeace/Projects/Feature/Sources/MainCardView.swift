94a1b67306bf7f0115844743a89948910fd312f4
saltCastle77 <hysung01@gmail.com>
2024-02-04 12:43:14 +0900

    add: 테스트용 뷰 작업완료

---
+++ OPeace/Projects/Feature/Sources/MainCardView.swift
@@ -0,0 +1,33 @@
+//
+//  MainView.swift
+//  Feature
+//
+//  Created by 염성훈 on 2/4/24.
+//  Copyright © 2024 Yeom. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainCardView: View {
+    
+    var number: Int
+    var content: String
+    @Binding var path: [ViewContent]
+    
+    var body: some View {
+        Text("메인카드 뷰")
+    }
+    
+    public init (
+        number: Int,
+        content: String,
+        path: Binding<[ViewContent]>
+    ) {
+        self.number = number
+        self.content = content
+        self._path = path
+    }
+    
+    
+}
--- OPeace/Projects/Feature/Sources/OnBoardingView.swift
+++ OPeace/Projects/Feature/Sources/OnBoardingView.swift
@@ -11,15 +11,46 @@ import SwiftUI
 import ComposableArchitecture
 
 public struct OnBoardingView: View {
+    
+    var number: Int
+    var content: String
+    @Binding var path: [ViewContent]
+    
     public var body: some View {
-        Text("안녕하세요?")
+        Text("안녕하세요? \(number)" )
+        Text("전달된 데이데: \(content)")
+        Button {
+            path.append(ViewContent(number: 123, content: "mainCardTest"))
+        } label: {
+            Text("다음 메인 카드뷰 테스트 화면 ")
+        }
+        .navigationDestination(for: ViewContent.self) { next in
+            OnBoardingView(number: next.number, content: next.content, path: $path)
+        }
+
     }
     
-    public init () {
-        
+    public init (
+        number: Int,
+        content: String,
+        path: Binding<[ViewContent]>
+    ) {
+        self.number = number
+        self.content = content
+        self._path = path
     }
 }
 
-#Preview {
-    OnBoardingView()
+//#Preview {
+//    OnBoardingView(number: 0, content: "안녕하세요 test", path: .)
+//}
+
+public struct ViewContent: Hashable {
+    public let number: Int
+    public let content: String
+    
+    public init(number: Int, content: String) {
+        self.number = number
+        self.content = content
+    }
 }
